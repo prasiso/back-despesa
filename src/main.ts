5d9d8d5bf1af0ac6dcb9c55ce6b49fd544c4a3d1
Kelwin de Souza <kelwinsimoesdesouza@gmail.com>
2025-05-28 10:56:03 -0300

    chore: add documentation

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
-
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { version } from '../package.json';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+    .setTitle('Api Despesa')
+    .setDescription('Api criada para computar a despesa')
+    .setVersion(String(version))
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config)
+  SwaggerModule.setup('documentation', app, documentFactory)
   app.useGlobalPipes(
     new ValidationPipe({
       transform: true,
