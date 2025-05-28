4efd7297368322f2c2ded026a2160ad586a89551
Kelwin de Souza <kelwinsimoesdesouza@gmail.com>
2025-05-28 03:10:32 -0300

    chore: add prisma in project and app module

---
+++ src/database/prisma.service.ts
@@ -0,0 +1,7 @@
+import { OnModuleInit } from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
+export class PrismaService extends PrismaClient implements OnModuleInit {
+  async onModuleInit() {
+    await this.$connect();
+  }
+}
