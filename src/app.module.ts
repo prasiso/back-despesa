acd64ed242fc065d11376a05861c2bc34aa389b9
Kelwin de Souza <kelwinsimoesdesouza@gmail.com>
2025-05-28 15:28:41 -0300

    feat: create generate token of jwt

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { PrismaModule } from './database/prisma.module';
 import { ExpenseModule } from './modules/expense/expense.module';
+import { AuthModule } from './modules/auth/auth.module';
+import { JwtModule } from 'src/shared/services/jwt/jwt.module';
 
 @Module({
-  imports: [PrismaModule, ExpenseModule],
+  imports: [PrismaModule, ExpenseModule, AuthModule, JwtModule],
 })
 export class AppModule {}
---
+++ src/modules/auth/auth.controller.ts
@@ -0,0 +1,14 @@
+import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { ApiTags } from '@nestjs/swagger';
+
+@Controller('auth')
+@ApiTags('auth')
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
+  @Post()
+  create() {
+    return this.authService.create();
+  }
+}
